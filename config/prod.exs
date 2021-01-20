6eb50f4d034f69efe5aa1ead9ce851117fa771f9
Jon Carstens <jjcarstens@me.com>
2021-01-19 18:48:45 -0700

    this should work

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :disc_golf, DiscGolfWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  live_view: [signing_salt: {:system, "LIVEVIEW_SIGNING_SALT"}]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/runtime.exs
+++ config/runtime.exs
@@ -4,6 +4,5 @@ config :disc_golf, DiscGolfWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: "dic.golf", port: 443],
-  secret_key_base: {:system, "SECRET_KEY_BASE"},
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  live_view: [signing_salt: {:system, "LIVEVIEW_SIGNING_SALT"}]
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  live_view: [signing_salt: System.get_env("LIVEVIEW_SIGNING_SALT")]
