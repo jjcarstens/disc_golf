d15289e27e1ccfc97de0b74ceee22d138b1298e5
Jon Carstens <jjcarstens@me.com>
2021-01-19 18:15:09 -0700

    add some security

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,6 @@ import Config
 config :disc_golf, DiscGolfWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: "dic.golf", port: 443],
+  live_view: [signing_salt: {:system, "LIVEVIEW_SIGNING_SALT"}],
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
